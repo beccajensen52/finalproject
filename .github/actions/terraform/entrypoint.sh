7753357b25940dff82e979f41443d902d4e2ae72
Rebecca Jensen <rebeccajensen@unomaha.edu>
2026-04-22 21:47:24 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+set -e
 
+export ARM_CLIENT_ID="$INPUT_ARM_CLIENT_ID"
+export ARM_CLIENT_SECRET="$INPUT_ARM_CLIENT_SECRET"
+export ARM_SUBSCRIPTION_ID="$INPUT_ARM_SUBSCRIPTION_ID"
+export ARM_TENANT_ID="$INPUT_ARM_TENANT_ID"
+export STATE_KEY="$INPUT_STATE_KEY"
+export TF_STAGE="$INPUT_TF_STAGE"
+
+echo "Running terraform for stage: $TF_STAGE"
+
+cd "$GITHUB_WORKSPACE/$TF_STAGE"
+
+terraform version
+terraform init
+terraform validate
+terraform plan
