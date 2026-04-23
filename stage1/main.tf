9f840eeaa3e017623cfff5a545abb368694f8794
Rebecca Jensen <rebeccajensen@unomaha.edu>
2026-04-22 20:25:44 -0500

    Create main.tf

---
+++ stage1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rj-acmp-final"
+    storage_account_name = "acmp2400@storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resources "azurerm_container_registry" "teacher-acr" {
+  name = "acrteacheracmp2400"
+  resource_group_name = "rj-teacher"
+  location = "Central US"
+  skw = "Basic"
+  admin_enabled = false
+}
