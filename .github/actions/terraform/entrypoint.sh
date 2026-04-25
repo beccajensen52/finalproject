9ab5e4209f947a82eba449eb021bb1d4d26f9f98
Rebecca Jensen <rebeccajensen@unomaha.edu>
2026-04-24 21:55:32 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -15,6 +15,9 @@ if [[ "$TF_STAGE" == "stage1" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 elif [[ "$TF_STAGE" == "stage2" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} import azurerm_container_registry.rebeccajensen-acr "/subscriptions/${INPUT_ARM_SUBSCRIPTION_ID}/resourceGroups/rg-rebeccajensen/providers/Microsoft.ContainerRegistry/registries/acrrebeccajensenacmp2400" || true
+  terraform -chdir=${INPUT_TF_STAGE} import azurerm_container_group.aci-rebeccajensen-acmp "/subscriptions/${INPUT_ARM_SUBSCRIPTION_ID}/resourceGroups/rg-rebeccajensen/providers/Microsoft.ContainerInstance/containerGroups/aci-rebeccajensen-acmp" || true
+  terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
   terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
 elif [[ "$TF_STAGE" == "stage3" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
